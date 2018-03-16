9f56eb572457e726e6ca4cea8870a6cce916737e
Álvaro Valencia <avalenciaparra@gmail.com>
2018-03-16 15:05:31 +0100

    feat: created aggregate endpoint in results api
    Created an endpoitn to perform aggregations on a field over another one.

--- routes/results.js
+++ routes/results.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var q2m = require('query-to-mongo');
+var mongoose = require('mongoose');
 
 var router = express.Router();
 
@@ -7,7 +8,7 @@ var Result = require('../schemas/result')
 var Instance = require('../schemas/instance')
 
 /**
- * RETRIEVE all results by instance id paginated
+ * RETRIEVE all results by instance id that match the specified query paginated
  * */
 router.get('/:instanceId', function (req, res, next) {
     var instanceId = req.params.instanceId;
@@ -19,7 +20,6 @@ router.get('/:instanceId', function (req, res, next) {
     // Let's delete the fields that wee won't use for filtering
     delete query.page;
     delete query.limit;
-    delete query.instanceId;
 
     // Parse de query to MongoDB format
     query = q2m(query);
@@ -41,7 +41,7 @@ router.get('/:instanceId', function (req, res, next) {
             var notIncluded = fieldsToIndex.filter(f => !instanceMappedFieldsSet.has(f));
             var isSubset = notIncluded.length === 0;
 
-            if(isSubset){
+            if (isSubset) {
                 var toIndex = {};
                 Object.keys(Object.assign(query.criteria, query.options.sort)).forEach(function (key) {
                     toIndex[key] = 1;
@@ -59,12 +59,82 @@ router.get('/:instanceId', function (req, res, next) {
                     }).catch(next);
                 }).catch(next);
             } else
-                res.status(400).send({error: "You have query some fields that are not declared in your instance as in, out or ot",
-                    fields: notIncluded});
+                res.status(400).send({
+                    error: "You have queried some fields that are not declared in your instance as in, out or ot",
+                    fields: notIncluded
+                });
         } else
             res.status(404).send({error: "The instance with _id " + instanceId + " does not exist."});
-
     }).catch(next);
+});
+
+/**
+ * RETRIEVE all results by instance id performing the aggregate operation
+ * */
+/*
+* TODO: validar la operación a realizar
+* */
+router.get('/aggregate/:instanceId', function (req, res, next) {
+    var instanceId = req.params.instanceId;
+    var query = req.query;
+
+    // Pagination information
+    var pagination = {page: parseInt(query.page) || 1, limit: parseInt(query.limit) || 10};
+
+    // Let's delete the fields that wee won't use for filtering
+    delete query.page;
+    delete query.limit;
+
+    if (!query.groupField || !query.op || !query.opField)
+        res.status(404).send({'error': 'You must specify the following information: groupField, op and opField.'});
+
+    var fieldsToIndex = [query.groupField];
+
+    // Let's check whether the instance exists and if the fields to query also exist
+    Instance.findById(instanceId).then(function (instance) {
+        if (instance) {
+            // Generate a list with all the mapped field of this instance. We need to concat the in., out., or ot.
+            var instanceMappedFields = [];
+            if (instance.in) instanceMappedFields = instanceMappedFields.concat(instance.in.map(s => 'in.' + s));
+            if (instance.out) instanceMappedFields = instanceMappedFields.concat(instance.out.map(s => 'out.' + s));
+            if (instance.ot) instanceMappedFields = instanceMappedFields.concat(instance.ot.map(s => 'ot.' + s));
+            var instanceMappedFieldsSet = new Set(instanceMappedFields);
+
+            // Check if the queried fields (fieldsToIndex) are contained in the instance mapped fields
+            var notIncluded = fieldsToIndex.filter(f => !instanceMappedFieldsSet.has(f));
+            var isSubset = notIncluded.length === 0;
+
+            if (isSubset) {
+                var toIndex = {};
+                fieldsToIndex.forEach(function (key) {
+                    toIndex[key] = 1;
+                });
+
+                // It will create the indexes only if they don't exist.
+                Result.collection.createIndex(toIndex, {partialFilterExpression: {instanceId: instanceId}}).then(function (result) {
+                    // Finally execute the query
+                    Result.aggregate([
+                            {$match: {instanceId: new mongoose.Types.ObjectId(instanceId)}},
+                            {
+                                $group: {
+                                    _id: '$' + query.groupField,
+                                    result: JSON.parse("{\"$"+query.op+"\": \"$"+ query.opField+"\"}")
+                                }
+                            }
+                        ]
+                    ).then(function (elements) {
+                        res.send(elements);
+                    }).catch(next);
+                }).catch(next);
+            } else
+                res.status(400).send({
+                    error: "You have queried some fields that are not declared in your instance as in, out or ot",
+                    fields: notIncluded
+                });
+        } else
+            res.status(404).send({error: "The instance with _id " + instanceId + " does not exist."});
+    }).catch(next);
+
 });
 
 /**
