43adecde535db138d9829455c79c497c390c50e4
Álvaro Valencia <avalenciaparra@gmail.com>
2018-04-10 10:57:29 +0200

    refactor: changes iuncludes by indexof

--- routes/instances.js
+++ routes/instances.js
@@ -158,7 +158,7 @@ router.post('/run/:id', function (req, res, next) {
             }, function (error, response, body) {
                 console.log("imprimiendo body");
                 console.log(body);
-                if(body.includes('Unauthorized')){
+                if(body.indexOf('Unauthorized') > -1){
                     res.status(401).send({error: "Unauthorized to access to the Spark cluster. Please renew the auth token."});
                 } else {
                     // We use the body parameter in order to check if there was an error.
@@ -240,8 +240,7 @@ router.get('/status/:id', function (req, res, next) {
                     headers: {'Authorization': 'token=eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzeUY1VE9TemRsSTQ1UTF4c3B4emVvR0JlOWZOeG05bSIsImVtYWlsIjoiYXZhbGVuY2lhcGFycmFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTUyMzc4MDQwNSwiaWF0IjoxNTIzMzQ4NDA1LCJpc3MiOiJodHRwczovL2Rjb3MuYXV0aDAuY29tLyIsInN1YiI6ImdpdGh1YnwxMDI4MDg2MiIsInVpZCI6ImF2YWxlbmNpYXBhcnJhQGdtYWlsLmNvbSJ9.k6oFjVoWHomd4w6-etkhZ0jUC4kGeDhgQZ76WoXh9y0'}
                 }, function (error, response, body) {
                 if (!error) {
-
-                    if(body.includes('Unauthorized')){
+                    if(body.indexOf('Unauthorized') > -1){
                         res.status(401).send({error: "Unauthorized to access to the Spark cluster. Please renew the auth token."});
                     } else {
                         try {
