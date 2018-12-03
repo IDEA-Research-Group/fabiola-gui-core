4615165bf2d631b9717dd20268625b9082ddd82b
Álvaro Valencia <avalenciaparra@gmail.com>
2018-12-03 11:08:00 +0100

    refactor: added auth token specification in config.yml

--- routes/instances.js
+++ routes/instances.js
@@ -154,7 +154,7 @@ router.post('/run/:id', function (req, res, next) {
             request.post({
                 url: config.fabiola.spark.submissionsUri + '/create',
                 json: bodyReq,
-                headers: {'Authorization': 'token=eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzeUY1VE9TemRsSTQ1UTF4c3B4emVvR0JlOWZOeG05bSIsImVtYWlsIjoiYXZhbGVuY2lhcGFycmFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTUyMzc4MDQwNSwiaWF0IjoxNTIzMzQ4NDA1LCJpc3MiOiJodHRwczovL2Rjb3MuYXV0aDAuY29tLyIsInN1YiI6ImdpdGh1YnwxMDI4MDg2MiIsInVpZCI6ImF2YWxlbmNpYXBhcnJhQGdtYWlsLmNvbSJ9.k6oFjVoWHomd4w6-etkhZ0jUC4kGeDhgQZ76WoXh9y0'}
+                headers: {'Authorization': 'token=' + config.fabiola.spark.authToken}
             }, function (error, response, body) {
                 if(!error){
                     if(body.toString().indexOf('Unauthorized') > -1){
